dabb9f7c27d41ceca65b58e5af0fd16f27a1339f
LahiruMS <65674871+LahiruMS@users.noreply.github.com>
2024-11-04 09:03:49 +0530

    Add files via upload

---
+++ Tiktok_clone/App/Tiktok_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tiktok_cloneApp.swift
+//  Tiktok_clone
+//
+//  Created by Lahiru Madushan on 2024-10-14.
+//
+
+import SwiftUI
+
+@main
+struct Tiktok_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tiktok_clone/Core/Feed/FeedCell.swift
@@ -0,0 +1,122 @@
+//
+//  FeedCell.swift
+//  Tiktok_clone
+//
+//  Created by Lahiru Madushan on 2024-10-22.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    let post: Int
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.pink)
+                .containerRelativeFrame([.horizontal, .vertical])
+                .overlay {
+                    Text("Post \(post)")
+                        .foregroundStyle(.white)
+                }
+            
+            VStack {
+                Spacer()
+                
+                HStack(alignment: .bottom) {
+                    VStack(alignment: .leading) {
+                        Text("Lahiru Madushan")
+                            .fontWeight(.semibold)
+                        
+                        Text("Rocket ship prepare for takeoff!!")
+                    }
+                    .foregroundStyle(.white)
+                    .font(.subheadline)
+                    
+                    Spacer()
+                    
+                    VStack(spacing: 28) {
+                        
+                        Circle()
+                            .frame(width: 48, height: 48)
+                            .foregroundStyle(.gray)
+                        
+                        Button {
+                        
+                        } label: {
+                            VStack {
+                                Image(systemName: "heart.fill")
+                                    .resizable()
+                                    .frame(width: 28, height: 28)
+                                    .foregroundColor(.white)
+                                
+                                Text("27")
+                                    .font(.caption)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                        }
+                        
+                        
+                        Button {
+                        
+                        } label: {
+                            VStack {
+                                Image(systemName: "ellipsis.bubble.fill")
+                                    .resizable()
+                                    .frame(width: 28, height: 28)
+                                    .foregroundColor(.white)
+                                
+                                Text("27")
+                                    .font(.caption)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                        }
+                        
+                        Button {
+                        
+                        } label: {
+                        
+                            VStack {
+                                Image(systemName: "bookmark.fill")
+                                    .resizable()
+                                    .frame(width: 22, height: 28)
+                                    .foregroundColor(.white)
+                                
+                                Text("27")
+                                    .font(.caption)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                            
+                        }
+                        
+                        Button {
+                        
+                        } label: {
+                          
+                            VStack {
+                                Image(systemName: "arrowshape.turn.up.right.fill")
+                                    .resizable()
+                                    .frame(width: 28, height: 28)
+                                    .foregroundColor(.white)
+                                
+                                Text("27")
+                                    .font(.caption)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                        }
+                    }
+
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    FeedCell(post: 2)
+}
